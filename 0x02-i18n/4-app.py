41b67da60b11fd96f68410add30404367ab45cb1
Grey <greywhat2002@gmail.com>
2024-08-27 14:56:49 +0200

    added mock login system and changes template depending on url parameters

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -30,7 +30,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """simple main index route"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+This is an example app for the 0x02-i18n project.
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+def get_user():
+    """return user dictionary else None"""
+    if (request.args.get('login_as')
+       and int(request.args.get('login_as')) in users):
+        print(users.get(int(request.args.get('login_as'))))
+        return users.get(int(request.args.get('login_as')))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """get user and set global user"""
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    if (request.args.get('locale')
+       and request.args.get('locale') in app.config['LANGUAGES']):
+        return request.args.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """simple main index route"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
