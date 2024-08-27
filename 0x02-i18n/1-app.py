b4f234235121dc760fc9f297793f2abc87a68907
Grey <greywhat2002@gmail.com>
2024-08-27 11:22:34 +0200

    instatiated babel object in app and a confugiration Class

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This is an example app for the 0x02-i18n project.
+"""
+from flask import Flask, render_template, Response
+from flask_babel import Babel
+
+
+class Config(object):
+    """config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> Response:
+    """simple main index route"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
