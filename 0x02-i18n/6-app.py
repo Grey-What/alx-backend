83bb5030dbddc08219ed3bc0a8afad88fcf12eea
Grey <greywhat2002@gmail.com>
2024-08-27 15:51:08 +0200

    moved user table

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -6,14 +6,6 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
 class Config(object):
     """config class"""
     LANGUAGES = ['en', 'fr']
@@ -24,6 +16,14 @@ class Config(object):
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 
 
 def get_user():
